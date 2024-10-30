b3ef85a4317e724412b072cffab9226d8d1d4f21
K-Marple <taeshinoberu@outlook.com>
2024-10-30 11:12:26 -0700

    form error msg fix

--- scripts/form-popup.js
+++ scripts/form-popup.js
@@ -1,5 +1,25 @@
 const inputEl = document.querySelector("input");
+const uInput = document.getElementById("username");
+const pInput = document.getElementById("password");
+const ucInput = document.querySelector(".user");
+const pcInput = document.querySelector(".pass");
 
 inputEl.addEventListener("invalid", e => {
+    e.preventDefault();
+})
+
+uInput.addEventListener("invalid", e => {
+    e.preventDefault();
+})
+
+pInput.addEventListener("invalid", e => {
+    e.preventDefault();
+})
+
+ucInput.addEventListener("invalid", e => {
+    e.preventDefault();
+})
+
+pcInput.addEventListener("invalid", e => {
     e.preventDefault();
 })
